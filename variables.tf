cd326cb8e858441078d3e870986777131d73d060
Daniel Salvador Andrade Davila <daniel.andrade.sd@gmail.com>
2025-03-07 20:29:44 -0600

    updated project

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "location where all resources will be deploy"
+  default     = "eastus"
+}
+
+variable "resource_group_name" {
+  description = "resource group name"
+  default     = "rg-azure-lab"
+}
